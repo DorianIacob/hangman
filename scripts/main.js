6c3bea46f92a4ed703251083c23b9cf2e93fb7af
DorianIacob <iacob.dorian.gr@gmail.com>
2021-07-07 12:39:56 +0300

    Implemented all game's functions
    - clean the unnecessary code
    - finished checkLetter function
    - implemented gameWin, gameOver, createRestartButton, restartGame, displayAttempts and removeAllChildNode functions
    - slightly modified the HTML

--- scripts/main.js
+++ scripts/main.js
@@ -1,44 +1,33 @@
-const startButton = document.getElementById("start");
+const instruction1 = document.getElementById("instruction1");
+const instruction2 = document.getElementById("instruction2");
+const instruction3 = document.getElementById("instruction3");
 let guessedWord = "";
 let attemptsLeft = 10;
 let lettersLeft = 0;
 
-startButton.onclick = startGame;
-
-function displayAttempts(element, value) {
-	element.textContent = "You have " + value + " more attempts left";
-}
+document.getElementById("start").onclick = startGame;
 
 function startGame() {
-	const enteredWord = document.getElementById("word");
-	guessedWord = enteredWord.value;
+	guessedWord = document.getElementById("word").value;
 	lettersLeft = guessedWord.length;
 	if (lettersLeft !== 0) {
 		guessedWord = guessedWord.toUpperCase();
-		const gameStart = document.getElementById("gameStart");
-		gameStart.removeChild(enteredWord);
-		gameStart.removeChild(startButton);
-		const instructions = document.getElementById("instructions");
-		instructions.textContent = "Try to guess the word!"
-		const infoAttempts = document.createElement("p");
-		displayAttempts(infoAttempts, attemptsLeft);
-		const informUser = document.getElementById("informUser");
-		informUser.appendChild(infoAttempts);
+		removeAllChildNodes(document.getElementById("gameStart"));
+		instruction1.textContent = "Try to guess the word!";
+		displayAttempts();
 		for (let i = 1; i <= guessedWord.length; ++i) {
 			const letterBox = document.createElement("input");
 			letterBox.type = "text";
 			letterBox.class = "letters";
 			letterBox.id = i;
 			letterBox.readOnly = true;
-			const lettersOutputField = document.getElementById("lettersOutputField");
-			lettersOutputField.appendChild(letterBox);
+			document.getElementById("lettersOutputField").appendChild(letterBox);
 		}
 		for (let i = 0; i < 26; ++i) {
 			const letterButton = document.createElement("button");
 			letterButton.id = letterButton.textContent = String.fromCharCode(65 + i);
 			letterButton.onclick = function () {checkLetter(letterButton.textContent)};
-			const letterButtonsField = document.getElementById("letterButtonsField");
-			letterButtonsField.appendChild(letterButton);
+			document.getElementById("letterButtonsField").appendChild(letterButton);
 		}
 	}
 }
@@ -50,15 +39,74 @@ function checkLetter(letter) {
 			letterFound = 1;
 			const letterBox = document.getElementById(i + 1);
 			letterBox.value = letter;
-			document.getElementById(letter).style.color = "green";
+			document.getElementById(letter).style.backgroundColor = "green";
+			document.getElementById(letter).disabled = true;
 			--lettersLeft;
 			if (lettersLeft === 0) {
-				winGame();
+				gameWin();
 			}
 		}
 	}
 	if (letterFound === 0) {
-		document.getElementById(letter).style.color = "red";
+		document.getElementById(letter).style.backgroundColor = "red";
+		document.getElementById(letter).disabled = true;
 		--attemptsLeft;
+		if (attemptsLeft) {
+			displayAttempts();
+		} else {
+			gameOver();
+		}
+
+	}
+}
+
+function gameWin() {
+	instruction1.textContent = "Congratulations! You guessed the word!";
+	instruction2.textContent ="The word is: " + guessedWord;
+	instruction3.textContent = "Press \"Restart game\" if you want to play one more time";
+	removeAllChildNodes(document.getElementById("lettersOutputField"));
+	removeAllChildNodes(document.getElementById("letterButtonsField"));
+	createRestartButton();
+}
+
+function gameOver() {
+	instruction1.textContent = "Sorry, you lost...";
+	instruction2.textContent ="The word is: " + guessedWord;
+	instruction3.textContent = "Press \"Restart game\" if you want to play one more time";
+	removeAllChildNodes(document.getElementById("lettersOutputField"));
+	removeAllChildNodes(document.getElementById("letterButtonsField"));
+	createRestartButton();
+}
+
+function createRestartButton() {
+	const restartButton = document.createElement("button");
+	restartButton.textContent = "Restart game";
+	restartButton.id = "restartButton";
+	restartButton.onclick = restartGame;
+	document.getElementById("gameStart").appendChild(restartButton);
+}
+
+function restartGame() {
+	instruction1.textContent = "Type in a word and press the \"Start\" button";
+	instruction2.textContent = "";
+	instruction3.textContent = "";
+	document.getElementById("gameStart").removeChild(document.getElementById("gameStart").firstChild);
+	const inputWord = document.createElement("input");
+	inputWord.id = "word";
+	document.getElementById("gameStart").appendChild(inputWord);
+	const startButton = document.createElement("button");
+	startButton.textContent = "Start";
+	startButton.id = "start";
+	startButton.onclick = startGame;
+	document.getElementById("gameStart").appendChild(startButton);
+}
+
+function displayAttempts() {
+	instruction3.textContent = "You have " + attemptsLeft + " more attempts left";
+}
+
+function removeAllChildNodes(parent) {
+	while (parent.firstChild) {
+		parent.removeChild(parent.firstChild);
 	}
 }
